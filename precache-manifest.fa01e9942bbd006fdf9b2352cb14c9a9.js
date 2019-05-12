7a0ef675a4e8963045f48eb852f240dc9bc0d5d9
lin lifan <lifan1312@gmail.com>
2019-05-12 16:07:29 +0800

    Updates

---
+++ precache-manifest.fa01e9942bbd006fdf9b2352cb14c9a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f86e2ffe97a36d3984a",
+    "url": "/minesweeper/static/js/runtime~main.dd26f679.js"
+  },
+  {
+    "revision": "bfac41c6ff43cb4cad6d",
+    "url": "/minesweeper/static/js/main.24012bb9.chunk.js"
+  },
+  {
+    "revision": "c63d85398e1a472bd830",
+    "url": "/minesweeper/static/js/2.72912114.chunk.js"
+  },
+  {
+    "revision": "bfac41c6ff43cb4cad6d",
+    "url": "/minesweeper/static/css/main.5e982e33.chunk.css"
+  },
+  {
+    "revision": "7092a9a518cdcae73cdf98d420b381df",
+    "url": "/minesweeper/index.html"
+  }
+];
